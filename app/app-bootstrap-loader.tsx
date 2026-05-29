4d4f5550bc31272df1a8a78d64b244cf941dd3ff
Vladislav Andreev <orstudiocorp@gmail.com>
2026-05-29 20:26:33 +0500

    fix app

--- app/app-bootstrap-loader.tsx
+++ app/app-bootstrap-loader.tsx
@@ -1,10 +1,6 @@
 "use client"
 
-import dynamic from "next/dynamic"
-
-const AppBootstrap = dynamic(() => import("./vk-bootstrap").then((mod) => mod.AppBootstrap), {
-  ssr: false,
-})
+import { AppBootstrap } from "./vk-bootstrap"
 
 export function AppBootstrapLoader() {
   return <AppBootstrap />
